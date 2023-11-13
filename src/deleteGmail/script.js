da51a637479ba6965c16a892313749b27df7df68
shuhei-fujita <syuheifujita0117@gmail.com>
2023-11-13 12:55:53 +0900

    fix: update

--- src/deleteGmail/script.js
+++ src/deleteGmail/script.js
@@ -9,6 +9,7 @@ function deleteUnreadPromotions() {
   const maxThreadsPerBatch = 500; // Maximum number of threads to process at once
   const maxExecutionTime = 5 * 60 * 1000; // Maximum execution time in milliseconds (5 minutes)
   let start = 0;
+  let totalDeleted = 0; // 削除されたスレッドの合計数を追跡する変数
   const startTime = new Date().getTime();
 
   try {
@@ -18,13 +19,17 @@ function deleteUnreadPromotions() {
 
       deleteThreads(threads);
       logDeletion(threads.length);
+      totalDeleted += threads.length; // 合計を更新
 
       start += maxThreadsPerBatch;
       if (isTimeExceeded(startTime, maxExecutionTime)) break;
     }
   } catch (e) {
     logError(e);
+    return { 'error': e.toString() }; // エラー情報を返す
   }
+
+  return { 'deletedCount': totalDeleted }; // 削除されたメールの数を返す
 }
 
 /**
@@ -65,4 +70,57 @@ function isTimeExceeded(startTime, maxTime) {
  */
 function logError(error) {
   Logger.log(`An error occurred: ${error.toString()}`);
+}
+
+/** 
+HTTPリクエストを受けたときに deleteUnreadPromotions() 関数を実行
+その結果をHTTPレスポンスとして返す処理を実装します。
+今は 'e' を使用していませんが、将来的にリクエストの詳細を取得する場合に使えます。
+*/
+/**
+ * HTTP GET リクエストを処理する関数。
+ */
+function handleRequest(e) {
+  Logger.log(e);
+  if (e.parameter.action === 'deleteUnreadPromotions') {
+    var result = deleteUnreadPromotions();
+    if (result.error) {
+      return sendResponse(500, { 'status': 'error', 'message': result.error });
+    } else {
+      return sendResponse(200, { 'status': 'success', 'deletedCount': result.deletedCount });
+    }
+  } else {
+    return sendResponse(400, { 'status': 'error', 'message': 'Invalid action' });
+  }
+}
+
+/**
+ * HTTP GET リクエストを処理する関数。
+ */
+function doGet(e) {
+  try {
+    return handleRequest(e);
+  } catch (error) {
+    Logger.log(error);
+    return sendResponse(500, { 'status': 'error', 'message': error.toString() });
+  }
+}
+
+/**
+ * HTTP POST リクエストを処理する関数。
+ */
+function doPost(e) {
+  try {
+    return handleRequest(e);
+  } catch (error) {
+    Logger.log(error);
+    return sendResponse(500, { 'status': 'error', 'message': error.toString() });
+  }
+}
+
+// ステータスコードとレスポンスをJSONで返すヘルパー関数
+function sendResponse(statusCode, body) {
+  return ContentService.createTextOutput(JSON.stringify(body))
+    .setMimeType(ContentService.MimeType.JSON)
+    .setStatusCode(statusCode);
 }
